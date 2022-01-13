009823596455b27a1ccbbb91e53336aa6683c853
Daniel-darnell <darnelldanny1997@gmail.com>
2022-01-13 18:06:52 +0300

    user model

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+import email
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
@@ -27,6 +28,25 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser', True)
 
         return self._create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email=models.CharField(max_length=255, unique=True)
+    first_name=models.CharField(max_length=255, blank=True)
+    last_name=models.CharField(max_length=255, blank=True)
+    is_active=models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=False)
+    date_joined=models.DateTimeField(default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def save(self,*args, **kwargs):
+        super(User,self).save(*args, **kwargs)
+        return self
+
+
 
 
 
